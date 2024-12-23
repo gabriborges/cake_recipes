01af42d7e13ebf348a6486925fdd102160ecfd16
Gabriel Borges <carvalhobrgs@gmail.com>
2024-12-23 13:12:51 -0300

    Melhorias na interface

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -5,35 +5,43 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Cake Recipes'),
-        ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Pesquisar',
-                  prefixIcon: Icon(Icons.search),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                    borderSide: BorderSide.none,
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Receitas de Bolo'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Pesquisar',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide.none,
                 ),
+                filled: true,
+                fillColor: Colors.grey[200],
               ),
             ),
-            Container(
-              height: 300,
-              width: double.infinity,
-              child: CakeSimpleCard(
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                CakeSimpleCard(
                   imageUrl:
                       'https://thescranline.com/wp-content/uploads/2023/06/BLACK-FOREST-CAKE-S-01.jpg',
-                  title: 'Floresta Negra'),
-            )
-          ],
-        ));
+                  title: 'Floresta Negra',
+                  author: 'Autor Exemplo',
+                  readingTime: '5 min de leitura',
+                  cookingTime: '45 min',
+                ),
+                // Adicione mais CakeSimpleCard aqui conforme necessário
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
